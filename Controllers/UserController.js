3b752ed8812ea142c8ae8676f7fbd9e8585059d9
NikaQanashvili <qanashvilinika7@gmail.com>
2025-02-26 18:46:28 +0400

    add 2 new routes create user and get users

--- Controllers/UserController.js
+++ Controllers/UserController.js
@@ -1,15 +1,26 @@
+import fs from 'fs'
+
+const data = JSON.parse(fs.readFileSync('./data/users.json'));
+
+
 const getUsers = (req,res)=>{
     res.json({
         message:"you get all users",
-        data:'users'
+        data: data
     })
 }
 const createUser = (req,res)=>{
-    res.json({
-        message:"you get all users",
-        data:'users'
-    })
-}
+    if(!req.body.name||!req.body.email){
+        res.send('name and price are required!');
+    }
+    const newUser = {...req.body,id:Date.now()}
+         data.push(newUser)
+        fs.writeFileSync('./data/users.json',JSON.stringify(data))
+        res.json({
+            message:"new user is created",
+        })
+    }
+
 const editUser = (req,res)=>{
     res.json({
         message:"you get all users",
