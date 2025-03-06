eded70bcdad4ff71393daef3542b82438d61e538
NikaQanashvili <qanashvilinika7@gmail.com>
2025-03-06 15:45:16 +0400

    feat: add mongoose database

--- server.js
+++ server.js
@@ -1,4 +1,10 @@
 import app from "./app.js";
+import mongoose from "mongoose";
 
+mongoose.connect(process.env.DB_URL).then(()=>{
+    console.log('mongodb connected');
+}).catch((err)=>{
+    console.log('error',err.message);
+})
 
 app.listen(process.env.PORT)
