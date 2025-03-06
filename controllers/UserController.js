3952444657f9f7d0612c49c446832537d9082620
NikaQanashvili <qanashvilinika7@gmail.com>
2025-03-06 16:54:28 +0400

    feat: add function to create user in db

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -1,26 +1,32 @@
 import fs from 'fs'
+import User from '../models/User.js';
 
 const data = JSON.parse(fs.readFileSync('./data/users.json'));
 
 
-const getUsers = (req,res)=>{
+const getUsers = async(req,res)=>{
+    const users = await User.find({});
     res.json({
         message:"you get all users",
-        data: data
+        data:users
     })
 }
-const createUser = (req,res)=>{
-    if(!req.body.name||!req.body.email){
-        res.send('name and price are required!');
+const createUser =async (req,res)=>{
+    const {name,username,email} = req.body
+      const existingUser = await User.findOne({ email });
+    if(!name||!username||!email){
+        return res.status(400).json({message:'name, username and email are required',data:req.body})
     }
-    const newUser = {...req.body,id:Date.now()}
-         data.push(newUser)
-        fs.writeFileSync('./data/users.json',JSON.stringify(data))
-        res.json({
-            message:"new user is created",
-            data: newUser
-        })
+    if(existingUser){
+        return res.status(400).json({message:'user email already exists'})
     }
+    const newUser = new User({
+          ...req.body,
+          id: Date.now(),
+        });
+    await newUser.save()
+    res.json({message:'User created',data:newUser})
+}
 
 const editUser = (req,res)=>{
     const userIndex = data.findIndex(user=>user.id===parseInt(req.params.id))
